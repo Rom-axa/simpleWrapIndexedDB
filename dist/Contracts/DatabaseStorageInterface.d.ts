6482bf5db424dca43104d694214c9b30b67d23a1
Rom-axa <romaxa.new@gmail.com>
2023-02-16 00:14:49 +0200

    chaned generic types

--- dist/Contracts/DatabaseStorageInterface.d.ts
+++ dist/Contracts/DatabaseStorageInterface.d.ts
@@ -1,10 +1,10 @@
 import QueryBuilderInterface from "./QueryBuilderInterface";
-export default interface DatabaseStorageInterface extends QueryBuilderInterface {
-    find<T = Object>(key: string): Promise<T | null>;
-    insert<T = Object>(entity: T): Promise<Event>;
-    insertBatch<T = Object>(entities: T[]): Promise<Event>;
-    update<T = Object>(entity: T): Promise<Event>;
-    updateBatch<T = Object>(entities: T[]): Promise<Event>;
+export default interface DatabaseStorageInterface<T> extends QueryBuilderInterface<T> {
+    find(key: string): Promise<T | null>;
+    insert(entity: T): Promise<Event>;
+    insertBatch(entities: T[]): Promise<Event>;
+    update(entity: T): Promise<Event>;
+    updateBatch(entities: T[]): Promise<Event>;
     delete(key: string): Promise<Event>;
     deleteBatch(keys: string[]): Promise<Event>;
 }
--- dist/Contracts/QueryBuilderInterface.d.ts
+++ dist/Contracts/QueryBuilderInterface.d.ts
@@ -1,8 +1,8 @@
 import QueryResultInterface from "./QueryResultInterface";
-export default interface QueryBuilderInterface {
-    filter(callback: CallableFunction): QueryBuilderInterface;
-    limit(limit: number): QueryBuilderInterface;
-    offset(offset: number): QueryBuilderInterface;
-    desc(): QueryBuilderInterface;
-    get(): Promise<QueryResultInterface>;
+export default interface QueryBuilderInterface<T = Object> {
+    filter(callback: CallableFunction): QueryBuilderInterface<T>;
+    limit(limit: number): QueryBuilderInterface<T>;
+    offset(offset: number): QueryBuilderInterface<T>;
+    desc(): QueryBuilderInterface<T>;
+    get(): Promise<QueryResultInterface<T>>;
 }
